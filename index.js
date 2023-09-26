6b5662b59e96d03496e23708ee4202e81db0a95f
moussakaram <moussakaram2@gmail.com>
2023-09-26 12:26:53 +0300

    step 3

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 
 const express = require('express')
 const app = express()
-const port = 3002
+
 
 app.get('/', (req, res) => {
   res.send('ok')
 })
-app.listen(port, () => {
+app.get('/test', (req, res) => {
+  res.status(200).send({status: res.statusCode, message:"ok"})
+})
+app.get('/time', (req, res) => {
+  res.status(200).send({status: res.statusCode, message:"<Time>"})
+})
+
+
+
+app.listen(port = 3002, () => {
     console.log(`OK ${port}`)
   })
