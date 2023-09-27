b33905d5cc3938429605fde72834b7fef59ce82b
moussakaram <moussakaram2@gmail.com>
2023-09-27 06:46:02 +0300

    step 4

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 const express = require('express')
 const app = express()
+const time=((new Date()).getHours())+ " : "+((new Date()).getMinutes())
 
 
 app.get('/', (req, res) => {
@@ -10,8 +11,32 @@ app.get('/test', (req, res) => {
   res.status(200).send({status: res.statusCode, message:"ok"})
 })
 app.get('/time', (req, res) => {
-  res.status(200).send({status: res.statusCode, message:"<Time>"})
+  res.status(200).send({status: res.statusCode, message:time})
 })
+app.get('/hello/:id',(req, res)=>{
+const id=req.params.id;
+res.status(200).send({status: res.statusCode, message:id})
+})
+
+app.get('/search', (req, res) => {
+  const search = req.query.s;
+
+  if (!search) {
+      res.status(500).json({
+          status: 500,
+          error: true,
+          message: "you have to provide a search"
+      });
+  } else {
+      res.json({
+          status: 200,
+          message: "ok",
+          data: search
+      });
+  }
+});
+
+
 
 
 
